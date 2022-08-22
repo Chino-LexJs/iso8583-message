7594e4a8c1149f336579193b65d34c19946688ec
Chino-LexJs <fabiansotovillanueva@gmail.com>
2022-08-22 18:33:16 -0300

    Se eliminaron archivos no usados

--- src/util/utils_dataElements/utils.ts
+++
@@ -1,6 +0,0 @@
-import { fields } from "../utils_dataElements/fields";
-import { util_checkMTI } from "../utils_dataElements/util_checkMTI";
-import { util_header } from "../utils_dataElements/util_header";
-import { util_checkBitmap } from "../utils_dataElements/util_checkBitmap";
-
-export { fields, util_checkMTI, util_header, util_checkBitmap };
